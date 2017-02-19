430c627859fdda2905b2365a6438de1659012d40
MyBook <MyBook@192.168.1.135>
2017-02-19 18:48:20 +0100

    Creación del proyecto Borrado Muchos

--- BorradoMuchos/src/modelos/HibernateUtil.java
+++ BorradoMuchos/src/modelos/HibernateUtil.java
@@ -19,21 +19,21 @@ public class HibernateUtil {
     static
     {
         
-        try //inicializamos la variable sesion factory
+        try
         {
             sessionFactory=new Configuration().configure().buildSessionFactory();
            
         }catch(HibernateException he)
         {
-            System.err.append("Ocurrio un error en la inicializacion del sessionFactory");
-            throw new ExceptionInInitializerError(he);
+            System.err.println("Ocurrio un error en la inicializacion del sessionFactory "+he);
+            throw he;          
         }
-        
+       
     }
     
     public static SessionFactory getSessionFactory()
     {
-        return sessionFactory;
+          return sessionFactory; 
     }
     
 }
